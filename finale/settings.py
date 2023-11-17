b4e1d80f57c55063eb385f343457a8f5ddbfa22a
Joseph <indiekaj@gmail.com>
2023-11-17 16:55:43 +0300

    done

--- finale/settings.py
+++ finale/settings.py
@@ -12,7 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-4e8_4(g0ro9017g3n9jn6rwiwx7pxwe7*y4^b3g%gehx$ekk71'
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
