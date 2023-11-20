709082aa6c052508aa0d66ca8ca2f01076ea0280
Joseph <indiekaj@gmail.com>
2023-11-20 15:25:57 +0300

    Email setup

--- finale/settings.py
+++ finale/settings.py
@@ -88,12 +88,6 @@ DATABASES = {
     },
     
 }
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -141,3 +135,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_USE_TLS = True  
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_HOST_USER = os.getenv('EMAIL')  
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')  
+EMAIL_PORT = 587 
