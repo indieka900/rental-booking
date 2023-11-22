45f04c07c9d780f55ab158345ef6ebc5d4faaf63
Joseph <indiekaj@gmail.com>
2023-11-22 17:04:20 +0300

    view apartment

--- rental_app/views.py
+++ rental_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rental_app.models import Rooms, Apartments
+from django.contrib.auth.decorators import login_required
+from accounts.decorators import landlord_required, tenant_required
+from django.contrib.auth.decorators import permission_required
 
+@landlord_required
 def viewRoom(request, id):
     room = Rooms.objects.get(id=id)
     return render(request, 'app/detail_page.html',{'room':room})
 
+# @permission_required('user.is_active',login_url='/')
 def viewRooms(request):
     rooms = Rooms.objects.filter(booked=False)
     return render(request, 'app/rooms.html', {'rooms': rooms})
