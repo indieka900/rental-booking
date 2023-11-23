f26f0e3c346c4b99787b3152305598ec7db1ca07
Joseph <indiekaj@gmail.com>
2023-11-23 15:18:36 +0300

    search functionality

--- rental_app/urls.py
+++ rental_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from rental_app.views import (viewRoom,viewRooms,viewApartment,viewRooms_L,viewHistory)
+from rental_app.views import (viewRoom,viewRooms,
+                              viewApartment,viewRooms_L,
+                              viewHistory, result
+                              )
 
 app_name = 'rentals'
 
@@ -8,5 +11,6 @@ urlpatterns = [
     path('landlord\'s-rooms/',viewRooms_L, name='landlords-rooms'),
     path('booking-history/', viewHistory, name='booking-history'),
     path('room/<id>/',viewRoom, name='room'),
+    path('search-results', result, name='result' ),
     path('apartment/<int:id>/',viewApartment, name='apartment'),
 ]
--- rental_app/views.py
+++ rental_app/views.py
@@ -4,6 +4,7 @@ from accounts.models import Prospectivetenant
 from rental_app.models import Rooms, Apartments, Booking_History
 from django.contrib.auth.decorators import login_required
 from accounts.decorators import landlord_required, tenant_required
+from django.db.models import Q
 from django.contrib.auth.decorators import permission_required
 
 # @landlord_required
@@ -44,7 +45,22 @@ def viewHistory(request):
     history = Booking_History.objects.filter(user=tenant)
     return render(request, 'app/booking-history.html', {'histories':history})
 
+def result(request):
+    if request.method == 'GET':
+        query = request.GET.get('query')
+        if query:
+            rooms = Rooms.objects.filter(
+                Q(booked=False) & 
+                (
+                    Q(room_number__icontains=query) |
+                    Q(rent__icontains=query) | 
+                    Q(room_type__icontains=query) |
+                    Q(apartment__apartment_name__icontains=query) |
+                    Q(apartment__location__icontains=query)
+                )
+            )
+            return render(request, 'app/search.html', {'rooms': rooms,'query':query})
 
+        return render(request, 'app/search.html')
 
-    return render(request, 'change_password.html', {'form': form})
 # Create your views here.
