3ac54e128f71e40fb0b2eed7a86b277a2723144c
Joseph <indiekaj@gmail.com>
2023-12-29 17:13:56 +0300

    docker training

--- finale/settings.py
+++ finale/settings.py
@@ -91,8 +91,12 @@ DATABASES = {
                 "autocommit": True,
             }
     },
-    
 }
+
+'''
+
+
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
