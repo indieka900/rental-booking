a2aa4069e30f865821cbd23169c98b64136f2da9
Joseph <indiekaj@gmail.com>
2023-12-31 13:48:50 +0300

    vercel file

--- finale/wsgi.py
+++ finale/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for finale project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +6,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finale.settings')
 
 application = get_wsgi_application()
+
+app = application
